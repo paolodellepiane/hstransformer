503a24ed1bff5763b5747b7362a3f68e00f04dea
pdell <paolod29@gmail.com>
2020-04-07 19:35:17 +0200

    .

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+-- #region_c import
 import           Data.Aeson
 import           Data.Aeson.Types
 import           Data.List                      ( intercalate )
@@ -8,6 +9,7 @@ import           Data.Foldable
 import           Data.Vector                    ( ifoldr )
 import           System.FilePath.Glob
 import qualified Data.HashMap.Strict           as H
+-- #endregion
 
 main = do
     paths <- glob "*.json"
@@ -24,15 +26,14 @@ transform v prefix = case v of
     String s -> display prefix s
     Number n -> display prefix n
     Bool   b -> display prefix b
-    where varName = prefix ++ if prefix == "" then "" else "__"
+  where
+    varName = prefix ++ if prefix == "" then "" else "__"
+    display prefix s = prefix ++ " = " ++ show s
 
-display prefix s = prefix ++ " = " ++ show s
-
--- utils
+-- #region_c utils
 expect err = fromMaybe (error err)
 expectDecoded file = expect $ "can't decode " ++ (show file)
 strip what = filter ((/=) what)
 stripQuotes = strip '\"'
-(|>) :: a -> (a -> b) -> b
 (|>) a b = b a
-(<|) = ($)
+-- #endregion
