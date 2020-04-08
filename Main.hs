05401e45f88b1ee96bed0f0891d7dfe528ecaa69
pdell <paolod29@gmail.com>
2020-04-08 11:11:59 +0200

    .

--- Main.hs
+++ Main.hs
@@ -1,25 +1,9 @@
 -- #region imports
 module Main where
 
-import           Data.Aeson
-import           Data.Aeson.Types
-import           Data.List            ( intercalate )
-import           Data.Maybe
-import           Data.Foldable
-import           Data.Vector          ( ifoldr )
+import MyPrelude
 import qualified Data.Text                     as Text
-import           System.FilePath.Glob
 import qualified Data.HashMap.Strict           as Hash
--- #endregion
--- #region utils
-expect err = fromMaybe (error err)
-expectDecoded file = expect $ "can't decode " ++ (show file)
-replace from to = Text.unpack . Text.replace (Text.pack from) (Text.pack to) . Text.pack
-strip what = replace what ""
-trim what = filter $ (/=) what
-toValue = strip "String " . strip "Bool " . strip "Number " . show
-toKey s = (trim '\"' . show) s
-(|>) a b = b a
 -- #endregion
 
 main = do
